b129f38ec38bd1b78e907b7d9cf48618cbdd137f
kaijieshi7 <1638961687@qq.com>
2021-07-30 20:23:57 +0800

    change upsample from nearest to bilinear

--- SPADE/models/generator.py
+++ SPADE/models/generator.py
@@ -6,7 +6,8 @@ def generator(is_32, is_16, is_8, is_4, is_2, is_1, opt, z=None, trainable=True)
     spectral = False
 
     def up(d, name=''):
-        return flow.layers.upsample_2d(d, name=name)
+        # return flow.layers.upsample_2d(d, name=name)
+        return flow.layers.upsample_2d(d, name=name, interpolation='bilinear')
 
 
     if opt.use_vae:
